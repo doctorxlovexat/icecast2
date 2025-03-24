d0f1a89fcabafa5102bb283756609302893f46de
doctorxlovexat <ervin-star@hotmail.de>
2025-03-24 03:25:20 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,24 +1,38 @@
 #!/bin/sh
 
-env
-
-set -x
-
+# Postavljanje vrednosti u icecast.xml
 set_val() {
     if [ -n "$2" ]; then
         echo "set '$2' to '$1'"
-        sed -i "s/<$2>[^<]*<\/$2>/<$2>$1<\/$2>/g" /etc/icecast2/icecast.xml
+        sed -i "s|<$2>[^<]*</$2>|<$2>$1</$2>|g" /etc/icecast2/icecast.xml
     else
         echo "Setting for '$1' is missing, skipping." >&2
     fi
 }
 
-set_val $ICECAST_SOURCE_PASSWORD source-password
-set_val $ICECAST_RELAY_PASSWORD  relay-password
-set_val $ICECAST_ADMIN_PASSWORD  admin-password
-set_val $ICECAST_PASSWORD        password
-set_val $ICECAST_HOSTNAME        hostname
+# Direktno postavljanje vrednosti prema XML-u
+set_val "zizu" source-password
+set_val "zizu" relay-password
+set_val "gladijator" admin-user
+set_val "zizu" admin-password
+set_val "https://icecast2-dw8i.onrender.com" hostname
+set_val "100" clients
+set_val "2" sources
+set_val "524288" queue-size
+set_val "30" client-timeout
+set_val "15" header-timeout
+set_val "10" source-timeout
+set_val "1" burst-on-connect
+set_val "65535" burst-size
+set_val "8000" port
+set_val "access.log" accesslog
+set_val "error.log" errorlog
+set_val "3" loglevel
+set_val "10000" logsize
+set_val "./log" logdir
+set_val "./web" webroot
+set_val "./admin" adminroot
 
+# Pokretanje Icecast servera
 set -e
-
 sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
