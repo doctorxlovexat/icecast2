b5cd7b67be41c875722614869cd1d4d187fee808
doctorxlovexat <ervin-star@hotmail.de>
2025-03-29 17:06:49 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 # Pokreće Icecast unutar Docker kontejnera
 docker build -t my-icecast .  # Izgradi Docker image (ako je potrebno)
-docker run -d -p 8000:8000 --name icecast my-icecast  # Pokreće kontejner u pozadini, izlažući port 8000
+docker run -d -p 10000:10000 --name icecast my-icecast  # Pokreće kontejner u pozadini, izlažući port 10000
