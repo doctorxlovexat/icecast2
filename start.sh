5e5242a60dd1921bb263d617579d9d00cd367af5
doctorxlovexat <ervin-star@hotmail.de>
2025-03-30 01:41:41 +0100

    Delete start.sh

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Pokreće Icecast unutar Docker kontejnera
-docker build -t my-icecast .  # Izgradi Docker image (ako je potrebno)
-docker run -d -p 80:80 --name icecast my-icecast  # Pokreće kontejner u pozadini, izlažući port 10000
